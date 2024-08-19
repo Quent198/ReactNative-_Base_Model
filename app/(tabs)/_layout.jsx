9f8e3a1d840cb48695a1ef0371cba09a688785ff
Quentin <quentingraj@gmail.com>
2024-08-19 10:24:57 +0200

    ok

---
+++ app/(tabs)/_layout.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Slot, Stack } from "expo-router";
+import { Text } from "react-native";
+
+const RootLayout = () => {
+  return (
+    <>
+      <Stack>
+        <Stack.Screen name="index" options={{ headerShown: false }} />
+      </Stack>
+    </>
+  );
+};
+
+export default RootLayout;
---
+++ app/(tabs)/index.jsx
@@ -0,0 +1,13 @@
+import { Text, View } from "react-native";
+import React from "react";
+import { Link } from "expo-router";
+
+const RootLayout = () => {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text>Nine Quest !</Text>
+    </View>
+  );
+};
+
+export default RootLayout;
